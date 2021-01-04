53d195f06d0d194dd3ee8b107da61b081cef5f24
Aidan Murray <aidanhansm@gmail.com>
2021-01-03 20:19:52 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#show'
+  get 'auth/github', as: 'github_login'
+  get 'auth/:provider/callback', to: 'sessions#create'
+
+  get 'dashboard', to: 'dashboard#show'
+
+  get '/logout', to: 'sessions#destroy', as: :log_out
 end
