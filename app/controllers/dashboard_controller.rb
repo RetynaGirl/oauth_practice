ad1bdefc4ec9cce072044bd69129cc82073e5c06
Aidan Murray <aidanhansm@gmail.com>
2021-01-04 00:00:59 -0700

    Get repos from the repo facade

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
     if current_user
       @current_user = current_user
 
-
+      @public_repos, @private_repos = RepositoryFacade.all_repos(current_user)
     else
       render file: "#{Rails.root}/public/404", status: 404
       nil
